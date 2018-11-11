9c2b6fc42e7dabc9ab181ea8d5bfe0029f5e3541
kpeay <31359261+kpeay@users.noreply.github.com>
2018-11-11 18:36:38 -0500

    Okay...

---
+++ Commanders of the Galaxy/Assets/PlayerAnimController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour {
+
+    static Animator anim;
+    public float speed = 2.0f;
+    public float rotationSpeed = 75.0f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent <Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (TacticsMove.turn == false)
+        {
+            anim.SetBool("Running", false);
+        }
+        else
+        {
+            anim.SetBool("Running", true);
+        }
+	}
+}
